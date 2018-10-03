50f6bff72d60112c3ac0d71507727d65f447f50e
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-03 17:59:47 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ function displayRepositories() {
             <h2>${repo.name}</h2>
             <a href="${repo.html_url}">${repo.html_url}</a><br>
             <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
           </li>`;
       })
       .join('') +
